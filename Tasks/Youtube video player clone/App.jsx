79625f26694b8b1542d94f234b646effc5320792
Deepak_K <139794590+Dkghuser@users.noreply.github.com>
2025-07-13 20:00:59 +0530

    Update App.jsx

--- Tasks/Youtube video player clone/App.jsx
+++ Tasks/Youtube video player clone/App.jsx
@@ -1,1 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import VIDEO from './video.json'
+import Container from './YOUTUBE/Container'
+import Navbar from './YOUTUBE/Navbar'
+import YoutubeLogo from './assets/YouTube-Logo.png';
+
+const App = () => {
+
+  let [state, setState] = useState(VIDEO);
+  let [play, setPlay] = useState(state[0]);
+
+  let handlePlay=(lio)=>{
+      setPlay(lio);
+  }
+
+  return (
+    <div>
+      <Navbar>
+      </Navbar>
+      <Container state={state} play={play} run={handlePlay}></Container>
+    </div>
+  )
+}
+
+export default App
