59dff0c063d7b1dc9a08bc9f1d799f248f6c7d87
Deepak_K <139794590+Dkghuser@users.noreply.github.com>
2025-07-14 21:09:32 +0530

    Update Videoplayer.jsx

--- Tasks/Youtube video player clone/YOUTUBE/Videoplayer.jsx
+++ Tasks/Youtube video player clone/YOUTUBE/Videoplayer.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 const Videoplayer = ({play}) => {
   return (
     <div>
-        {/* <video height={600} width={900} controls autoPlay muted id='player' src={play}></video> */}
         <video controls autoPlay muted id='player' src={play.videoUrl}></video>
         <div className="video-info">
           <h2>{play.title}</h2>
